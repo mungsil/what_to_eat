45ac649a667fa0a244add56b997bfe2db51e4559
mungsil <thddmd2009@gmail.com>
2026-03-19 09:35:19 +0900

    create spring boot project

---
+++ src/main/kotlin/com/fdb/what_to_eat/WhatToEatApplication.kt
@@ -0,0 +1,11 @@
+package com.fdb.what_to_eat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatToEatApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatToEatApplication>(*args)
+}
