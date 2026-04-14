730684a73a26799ae05ff0ca2285834261868a42
mungsil <thddmd2009@gmail.com>
2026-04-14 10:17:59 +0900

    소스 코드 변경 시 리소스 사용량 확인을 위해 더미 코드 추가

--- src/main/kotlin/com/fdb/what_to_eat/ResourceUsageTest.kt
+++ src/main/kotlin/com/fdb/what_to_eat/ResourceUsageTest.kt
@@ -3,4 +3,13 @@ class ResourceUsageTest {
     fun test() {
         println("test") 
     }
+
+    fun coffee(ice : Int){
+        when {
+            ice == 1 -> println("얼음 하나")
+            ice == 2 -> println("얼음 둘")
+            ice == 3 -> println("얼음 셋") 
+            ice > 3 -> println("얼음 많이")
+        }
+    }
 }
