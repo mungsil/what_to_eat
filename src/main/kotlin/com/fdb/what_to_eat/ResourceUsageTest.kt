02fa54ee790f75b20b2f7d93d63d60b559d944f6
mungsil <thddmd2009@gmail.com>
2026-04-15 19:05:48 +0900

    Jenkins NMT 분석을 위한 더미 코드 추가

---
+++ src/main/kotlin/com/fdb/what_to_eat/ResourceUsageTest.kt
@@ -0,0 +1,15 @@
+class ResourceUsageTest { 
+
+    fun test() {
+        println("test") 
+    }
+
+    fun menu(num : Int){
+        when {
+            num == 1 -> println("아이스 아메리카노")
+            num == 2 -> println("아이스 바닐라 라떼에 버블 추가")
+            num == 3 -> println("따뜻한 카라멜마끼아또") 
+            num > 3 -> println("딸기 바나나 주스")
+        }
+    }
+}
