a6055e5d80b7706feec08b7d19794d227c0cc521
mungsil <thddmd2009@gmail.com>
2026-04-16 20:52:29 +0900

    Add mockMenu function for fake menu options
    Added a mockMenu function to provide fake menu options based on input.

--- src/main/kotlin/com/fdb/what_to_eat/ResourceUsageTest.kt
+++ src/main/kotlin/com/fdb/what_to_eat/ResourceUsageTest.kt
@@ -12,4 +12,13 @@ class ResourceUsageTest {
             num > 3 -> println("딸기 바나나 주스")
         }
     }
+    
+    fun mockMenu(num : Int){
+        when {
+            num == 1 -> println("가짜 아메리카노")
+            num == 2 -> println("가짜 라떼")
+            num == 3 -> println("가짜 카라멜마끼아또") 
+            num > 3 -> println("가짜 바나나 주스")
+        }
+    }
 }
